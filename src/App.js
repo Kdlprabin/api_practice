ad9c1b899a5b61262c46efd3a3787aac709dd28d
prabin <kdlprabin444@gmail.com>
2022-12-04 19:56:31 +0545

    get post put delete and simultaneous request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
 function App() {
+  const getData = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=1")
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+  const postData = () => {
+    axios
+      .post("https://jsonplaceholder.typicode.com/todos?_limit=5", {
+        title: "Learn react",
+        completed: false,
+      })
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+  const putData = () => {
+    axios
+      .put("https://jsonplaceholder.typicode.com/todos/1", {
+        title: "Update Todo",
+        completed: false,
+      })
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+  const deleteData = () => {
+    axios
+      .delete("https://jsonplaceholder.typicode.com/todos/1")
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+  const allData = () => {
+    axios
+      .all([
+        axios.get("https://jsonplaceholder.typicode.com/todos?_limit=1"),
+        axios.get("https://jsonplaceholder.typicode.com/todos?_limit=2"),
+      ])
+      .then(axios.spread((todo, posts) => console.log(posts)))
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="Btns">
+        <Button as="button" onClick={getData} size="lg" variant="primary">
+          Get
+        </Button>
+        <Button as="button" onClick={postData} size="lg" variant="success">
+          Post
+        </Button>
+        <Button as="button" onClick={putData} size="lg" variant="warning">
+          Put
+        </Button>
+        <Button as="button" onClick={deleteData} size="lg" variant="danger">
+          Delete
+        </Button>
+        <Button
+          as="button"
+          onClick={allData}
+          size="lg"
+          variant="outline-primary"
         >
-          Learn React
-        </a>
-      </header>
+          ALL
+        </Button>
+      </div>
+      <hr />
     </div>
   );
 }
